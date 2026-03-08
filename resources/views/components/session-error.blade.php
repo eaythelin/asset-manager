2b7b2199feaecdd535d43ab60d169a12a8ee5dba
Janna Mhay C.Guerrero <guerrerojnmh@gmail.com>
2026-03-08 17:13:56 +0800

    Fixed Toast issues

--- resources/views/components/session-error.blade.php
+++ resources/views/components/session-error.blade.php
@@ -1,6 +1,6 @@
 <!--This is for session errors!!-->
 @if(session('error'))
-  <div class="toast toast-center fixed top-10 right-0 z-50"
+  <div class="fixed top-10 left-1/2 -translate-x-1/2 z-50 pointer-events-none"
     x-data="{ show: true}" x-show="show" x-init="setTimeout(() => show = false, 3000)">
     <div class="alert alert-warning flex flex-row gap-5">
       <x-heroicon-o-exclamation-triangle class="size-5 sm:size-7"/>
--- resources/views/components/toast-success.blade.php
+++ resources/views/components/toast-success.blade.php
@@ -1,9 +1,9 @@
 @if(session('success'))
-  <div class="toast toast-center fixed top-10 right-0 z-50"
-    x-data="{ show: true}" x-show="show" x-init="setTimeout(() => show = false, 3000)">
-    <div class="alert alert-success flex flex-row gap-5">
-      <x-heroicon-o-check-circle class="size-5 sm:size-7" />
-      <span class = "sm:text-base font-medium">{{ session('success') }}</span>
+  <div class="fixed top-10 left-1/2 -translate-x-1/2 z-50 pointer-events-none"
+    x-data="{ show: true }" x-show="show" x-init="setTimeout(() => show = false, 3000)">
+    <div class="alert alert-success flex flex-row gap-5 pointer-events-none">
+        <x-heroicon-o-check-circle class="size-5 sm:size-7" />
+        <span class="sm:text-base font-medium">{{ session('success') }}</span>
     </div>
   </div>
 @endif

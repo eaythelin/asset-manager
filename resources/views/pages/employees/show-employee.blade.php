e5fa3c04379451ff26b483741292eed17ab83f09
Janna <guerrerojnmh@gmail.com>
2026-07-14 05:35:38 +0800

    updated Employee page

--- resources/views/pages/employees/show-employee.blade.php
+++ resources/views/pages/employees/show-employee.blade.php
@@ -8,21 +8,27 @@
 
   <x-view-page-header-card>
     <div class="flex flex-col md:flex-row md:items-center justify-between gap-6">
-      <div class="flex items-start gap-4">
+      <div class="flex items-center gap-4">
         <div class="hidden sm:flex items-center justify-center size-12 rounded-xl bg-indigo-50 text-indigo-600">
           <x-heroicon-s-user class="size-6"/>
         </div>
 
-        <div>
-          <div class="flex items-center gap-2 mb-1">
-            <span class="text-xs font-bold uppercase tracking-wider text-gray-400">Department: </span>
-            <span class="text-xs py-0.5 px-2 bg-gray-100 text-gray-600 rounded-md font-mono">
+        <div class="grow text-center sm:text-left">
+          <h2 class="text-2xl font-bold text-gray-800 mb-2">{{ $employee->name }}</h2>
+          <div class="flex items-center justify-center sm:justify-start gap-2 text-sm mt-1">
+            <x-heroicon-s-hashtag class="text-gray-500 size-4"/>
+            <span class="text-xs font-bold uppercase tracking-wider text-gray-400">Employee No.:</span>
+            <span class="text-sm py-0.5 px-2 bg-gray-100 text-gray-600 rounded-md font-mono">
+              {{ $employee->employee_no }}
+            </span>
+          </div>
+          <div class="flex items-center justify-center sm:justify-start gap-2 text-sm mt-1">
+            <x-heroicon-s-identification class="text-gray-500 size-4"/>
+            <span class="text-xs font-bold uppercase tracking-wider text-gray-400">Department:</span>
+            <span class="text-sm py-0.5 px-2 bg-gray-100 text-gray-600 rounded-md font-mono">
               {{ $employee->department->name }}
             </span>
           </div>
-          <h2 class="text-xl font-bold text-gray-900 leading-tight">
-            {{ $employee->name }}
-          </h2>
         </div>
       </div>
 
